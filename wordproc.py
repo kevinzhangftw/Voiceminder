65ff1246739b0c53e7843a7d1f987bc59a7f6112
Kevin Zhang <kevin.c.t.zhang@gmail.com>
2017-11-05 15:39:25 -0800

    reduced userinput for faster process and update wordproc for spacy functions

--- wordproc.py
+++ wordproc.py
@@ -1,9 +1,42 @@
-
 import sys
 import spacy
 
-userinput = open(sys.argv[1],'r').read() 
+userinput = open(sys.argv[1],'r').read()
 nlp = spacy.load('en')
 
-processedtext = nlp(userinput)
-print (processedtext)
+# print(userinput)
+doc = nlp(userinput)
+
+# print each sentence
+# for sent in doc.sents:
+#     print(sent)
+
+# speech tag
+# for token in doc:
+#     print('{} - {}'.format(token, token.pos_))
+
+# entity recognition
+# for ent in doc.ents:
+#     print('{} - {}'.format(ent, ent.label_))
+
+# noun chunks
+# for chunk in doc.noun_chunks:
+# 	print(chunk)
+
+# unigram log prob
+# for token in doc:
+#     print(token, ',', token.prob)
+
+# word comparsion
+# apples = doc[0]
+# oranges = doc[2]
+# boots = doc[6]
+# hippos = doc[8]
+# print(apples.similarity(oranges))
+# print(boots.similarity(hippos))
+
+# Print similarity between sentence and word 'korean'
+word = doc.vocab[u'fat']
+for sent in doc.sents:
+	print(sent.similarity(word))
+	
